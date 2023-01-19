4f75c3c6193c2e261aba274d4067f4287c10f15e
pahmtienduc <phamtienducmedia99@gmail.com>
2023-01-19 19:03:33 +0700

    update

--- bai012.c
+++ bai012.c
@@ -1,5 +1,5 @@
 // HỌC C CÙNG MÌNH
-// BÀI 12 :Tính tổng  và tích tất cả các “ ước số” của số nguyên dương n
+// BÀI 12 : Tính tổng và tích của số nguyên dương n
 #include <stdio.h>
 #include <conio.h>
 #include <math.h>
@@ -12,35 +12,34 @@ int main()
     // nhập n>0, n<=0 nhập lại
     do
     {
-        printf("\nnhap vao so n : ");
+        printf("\nnhap vao so n:");
         scanf("%d", &n);
-        // điều kiện nhập lại
+        // điều kiện nhập n
         if (n <= 0)
         {
-            printf("nhap lai n(n>0)");
+            printf("nhap lai n(n>0)!");
         }
     } while (n <= 0);
     // khai báo i duyệt các ước
     int i;
     // khai báo biến tổng và tích
     int tong = 0, tich = 1;
-    // dùng vòng lặp for hoặc while để tính
-    printf("uoc cua %d la ", n);
+    // dùng vòng lặp for or while để tính
+    printf("cac uoc cua %d la : ", n);
     for (i = 1; i <= n; i++)
     {
-        // kiểm tra điều kiện rồi tính
+        // điều kiện để tính
         if (n % i == 0)
         {
-            // in ra các ước
-            printf("%d", i);
-            // tính tổng và tích
+            // in ra các ước rồi tính
+            printf("\t%d", i);
             tong += i;
             tich *= i;
         }
     }
-    // in ra tỏng và tích
-    printf("\ntong=%d", tong);
-    printf("\ntich=%d", tich);
+    // in ra kết quả
+    printf("\ntong la = %d", tong);
+    printf("\ntich la = %d", tich);
     getch(); // dừng màn hình
     return 0;
 }
