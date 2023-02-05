98feb29fcef3f4fbb77ae9a4f2bbffc1d6b5798c
pahmtienduc <phamtienducmedia99@gmail.com>
2023-02-05 20:59:06 +0700

    upadate

--- bai023.c
+++ bai023.c
@@ -3,44 +3,43 @@
 // và tính số fibonaci 
 #include <stdio.h>
 #include <math.h>
-// hàm tính giai thừa
-int Giai_Thua(int n)
+// ham tinh gia thua
+int Tinh_GT(int n)
 {
-    if(n==0 || n==1)
-    {
-        return 1;
-    }
-    else 
-    {
-        return n*Giai_Thua(n-1);
-    }
-} 
-// hàm tính đệ quy 
+	if(n==0 || n==1)
+	{
+		return 1;
+	}
+	else 
+	{
+		return n*Tinh_GT(n-1);
+	}
+}
+// ham tinh Fibonaci
 int tinh_Fibonaci(int n)
 {
-    if(n==0)
-    {
-        return 0;
-    }
-    else if(n==1)
-    {
-        return 1;
-    }
-    else 
-    {
-        return tinh_Fibonaci(n-1) + tinh_Fibonaci(n-2);
-    }
+	if(n==0)
+	{
+		return 0;
+	}
+	else if(n==1)
+	{
+		return 1;
+	}
+	else 
+	{
+		return tinh_Fibonaci(n-1) + tinh_Fibonaci(n-2);
+	}
 }
 int main ()
 {
-    int n;
-    do 
-    {
-        printf("nhap vao so n (n>0)  :");
-        scanf("%d", &n);
-    }while(n<0);
-    // goi ham giai thua
-    printf("So %d! = %d", n, Giai_Thua(n));
-    printf("\nSo Fibonaci(%d) = %d", n, tinh_Fibonaci(n));
-    return 0;
+	int n;
+	do 
+	{
+		printf("nhap vao so n = ");
+		scanf("%d", &n);
+	}while(n<0);
+	printf("\nSo %d! = %d", n, Tinh_GT(n));
+	printf("\nSo Fibonaci(%d) = %d", n, tinh_Fibonaci(n));
+	return 0;
 }
